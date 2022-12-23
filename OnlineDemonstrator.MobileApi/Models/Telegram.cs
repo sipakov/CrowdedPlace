6795375279ff61d26dae80ae61d7b97cbf0b576d
ssipakov <ssipakov@PC0019>
2022-12-23 15:49:48 +0300

    #feature-14 Add hook to telegram by serilog

---
+++ OnlineDemonstrator.MobileApi/Models/Telegram.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace OnlineDemonstrator.MobileApi.Models
+{
+    public class Telegram
+    {
+        public string TelegramChatName { get; set; }
+        
+        public TelegramCredentials TelegramCredentials { get; set; }
+    }
+}
--- OnlineDemonstrator.MobileApi/Program.cs
+++ OnlineDemonstrator.MobileApi/Program.cs
@@ -1,8 +1,12 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyModel;
+using OnlineDemonstrator.MobileApi.Models;
 using Serilog;
 using Serilog.Events;
 using TelegramSink;
@@ -14,19 +18,25 @@ namespace OnlineDemonstrator.MobileApi
         [Obsolete("Obsolete")]
         public static void Main(string[] args)
         {
-            IConfiguration configSerilog = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json", true, true)
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false)
                 .Build();
+            
+            var telegramCredentials = new List<Telegram>();
+
+            config.GetSection("Telegram").Bind(telegramCredentials);
 
             var pathToLog = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "crowded-place", "log", "serilog-log-.txt");
 
             var configuration = new LoggerConfiguration()
-                .ReadFrom.Configuration(configSerilog)
+                .ReadFrom.Configuration(config)
                 .Enrich.FromLogContext()
                 .Enrich.WithMachineName()
                 .Enrich.WithProperty("App", "crowded-place")
                 .WriteTo.Console()
-                .WriteTo.TeleSink(telegramApiKey:"",telegramChatId: "", minimumLevel:LogEventLevel.Information)
+                .WriteTo.TeleSink(telegramApiKey: telegramCredentials.First(x => x.TelegramChatName == "crowded-place-logs").TelegramCredentials.TelegramApiKey,
+                    telegramChatId: telegramCredentials.First(x => x.TelegramChatName == "crowded-place-logs").TelegramCredentials.TelegramChatId,
+                    minimumLevel: LogEventLevel.Information)
                 .WriteTo.File(pathToLog, rollingInterval: RollingInterval.Day, rollOnFileSizeLimit: true, fileSizeLimitBytes: 104857600, retainedFileCountLimit: 31);
             
             Log.Logger = configuration.CreateLogger();
